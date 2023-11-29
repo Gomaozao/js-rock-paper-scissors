5904e640ce533eaff33798faf46eb6e9bff28f1c
Caio César Gomes Ferigolli <40327219+Gomaozao@users.noreply.github.com>
2023-11-29 19:19:14 -0300

    getComputerChoice Function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+function getComputerChoice() {
+    
+    if (getRandomInt(3) == 0) {
+        return 'Rock';
+    }
+    else if (getRandomInt(3) == 1) {
+        return 'Paper';
+    }
+    else {
+        return 'Scissors';
+    }
+    
+
+
+}
+
+console.log(getComputerChoice());
