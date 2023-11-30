765cc85019cca149fcaa35e8e31a49f97ca0ab83
Caio César Gomes Ferigolli <40327219+Gomaozao@users.noreply.github.com>
2023-11-30 19:48:35 -0300

    finished project

--- script.js
+++ script.js
@@ -2,21 +2,71 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-
 function getComputerChoice() {
-    
+
     if (getRandomInt(3) == 0) {
-        return 'Rock';
+        return 'rock';
     }
     else if (getRandomInt(3) == 1) {
-        return 'Paper';
+        return 'paper';
     }
     else {
-        return 'Scissors';
+        return 'scissors';
     }
-    
-
 
 }
 
-console.log(getComputerChoice());
+function rpsRound() {
+    playerSelection = prompt("Write Rock, Paper or Scissors:");
+    computerSelection = getComputerChoice();
+
+    if (computerSelection.toLowerCase() == 'rock' && playerSelection.toLowerCase() == 'scissors') {
+        return("Computer Won! Rock wins against Scissors!");
+    }
+    else if (computerSelection.toLowerCase() == 'rock' && playerSelection.toLowerCase() == 'paper') {
+        return("Human Won! Paper wins against Rock!");
+    }
+    else if (computerSelection.toLowerCase() == 'scissors' && playerSelection.toLowerCase() == 'rock') {
+        return("Human Won! Rock wins against Scissors!");
+    }
+    else if (computerSelection.toLowerCase() == 'paper' && playerSelection.toLowerCase() == 'rock') {
+        return("Computer Won! Paper wins against Rock!");
+    }
+    else if (computerSelection.toLowerCase() == 'paper' && playerSelection.toLowerCase() == 'scissors') {
+        return("Human Won! Scissors wins against Paper!");
+    }
+    else if (computerSelection.toLowerCase() == 'scissors' && playerSelection.toLowerCase() == 'paper') {
+        return("Human Won! Paper wins against Scissors!");
+    }
+    else if (computerSelection.toLowerCase() == 'rock' && playerSelection.toLowerCase() == 'rock') {
+        
+        return ("DRAW! PLAY AGAIN!");
+    }
+    else if (computerSelection.toLowerCase() == 'paper' && playerSelection.toLowerCase() == 'paper') {
+        return ("DRAW! PLAY AGAIN!");
+    }
+    else if (computerSelection.toLowerCase() == 'scissors' && playerSelection.toLowerCase() == 'scissors') {
+        return ("DRAW! PLAY AGAIN!");
+    }
+
+}
+
+function game() {
+
+    for(let i = 0; i < 5; i++) {
+
+        let result = rpsRound();
+
+        console.log(result);
+        
+        if (result == "DRAW! PLAY AGAIN!") {
+
+            i--;
+                    
+        }
+
+    }  
+}
+
+console.log("Begin Game!");
+game();
